9c74395f5e48f0b5381d80544609cac40cd3ea27
Evann Hall <halle15@email.franklin.edu>
2023-01-08 14:01:07 -0500

    working navbar

--- personalportfolio/src/main/java/mmd/personalportfolio/PersonalPortfolioController.java
+++ personalportfolio/src/main/java/mmd/personalportfolio/PersonalPortfolioController.java
@@ -2,9 +2,18 @@ package mmd.personalportfolio;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PersonalPortfolioController {
+	/*
+		@RequestMapping(value = "/css/{name}")
+		public String getCSS(@PathVariable String name) {
+			return "css/" + name + ".css";
+		}
+	*/
+	
 	  @GetMapping(value = "/")
 		public String index() {
 		  System.out.println("FUCK");
