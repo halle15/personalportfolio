916e20875d4004a214eda15a8e9b42218e4c95b7
Evann Hall <halle15@email.franklin.edu>
2023-02-06 17:37:45 -0500

    initial post functionality added

--- personalportfolio/src/main/java/mmd/personalportfolio/PersonalPortfolioController.java
+++ personalportfolio/src/main/java/mmd/personalportfolio/PersonalPortfolioController.java
@@ -10,45 +10,58 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PersonalPortfolioController {
-	
+
 	MessageRepository repo;
-	
-	PersonalPortfolioController(MessageRepository repo){
+
+	PersonalPortfolioController(MessageRepository repo) {
 		this.repo = repo;
 	}
 	/*
-		@RequestMapping(value = "/css/{name}")
-		public String getCSS(@PathVariable String name) {
-			return "css/" + name + ".css";
-		}
-	*/
-	
-	@RequestMapping(value = "/images/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<InputStreamResource> getImage(@PathVariable String name) throws IOException {
-        var imgFile = new ClassPathResource("images/" + name);
+	 * @RequestMapping(value = "/css/{name}") public String getCSS(@PathVariable
+	 * String name) { return "css/" + name + ".css"; }
+	 */
 
-        return ResponseEntity
-                .ok()
-                .contentType(MediaType.IMAGE_JPEG)
-                .body(new InputStreamResource(imgFile.getInputStream()));
-    }
-	
-	  @GetMapping(value = "/")
-		public String index() {
-			return "index";
-		}
-	  
-	  @GetMapping(value = "/home")
-		public String home() {
-			return "index";
-		}
-	  @GetMapping(value = "/contact")
+	@RequestMapping(value = "/images/{name}", produces = MediaType.IMAGE_JPEG_VALUE)
+	public ResponseEntity<InputStreamResource> getImage(@PathVariable String name) throws IOException {
+		var imgFile = new ClassPathResource("images/" + name);
+
+		return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG)
+				.body(new InputStreamResource(imgFile.getInputStream()));
+	}
+
+	@GetMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping(value = "/home")
+	public String home() {
+		return "index";
+	}
+
+	@GetMapping(value = "/contact")
 		public String contact() {
 			return "contact";
 		}
+
+	@PostMapping(value = "/message", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+		public Message putMessage(@RequestParam String name, @RequestParam String email, @RequestParam String message) {
+		  	Message inMsg = new Message(name, email, message);
+		
+			repo.save(inMsg);
+		    
+			return inMsg;
+		}
 	  
 }
+
+
+
