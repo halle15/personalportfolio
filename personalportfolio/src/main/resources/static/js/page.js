37abd39be8df0dfd5c4380c8ccf4c51ea9b25972
Evann Hall <105608620+halle15@users.noreply.github.com>
2023-08-10 09:45:21 -0400

    fixed read functionality, edge case, colored button

--- personalportfolio/src/main/resources/static/js/page.js
+++ personalportfolio/src/main/resources/static/js/page.js
@@ -55,13 +55,19 @@ function readMessage(id) {
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   xhr.setRequestHeader('Message-ID', id); // Set the ID as a request header
   xhr.onload = function() {
-    if (xhr.status !== 200) {
+	const readStatus = document.querySelector('[data-message-id="' + id + '"]').parentNode.parentNode.querySelector('.read-indicator');
+	if (readStatus.innerText === "Read") {
+		alert("Error: Message already read!");
+	}
+    else if (xhr.status !== 200) {
       // Error handling
       alert('Error marking the message as read.');
     }
     else{
-	const readStatus = document.querySelector('[data-message-id="' + id + '"]').parentNode.parentNode.querySelector('.read-indicator');
 	readStatus.innerText = "Read";
+	
+	const messageCount = document.querySelector('.message-count');
+	messageCount.innerText = messageCount.innerText - 1
 }	
   };
   xhr.send('id=' + encodeURIComponent(id));
